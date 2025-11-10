dd6ab41f77d7af45095ef05ddc52383377c7829b
Bezalel Mathew <bezalelmathew659@gmail.com>
2025-11-10 21:42:37 +0530

    Fix connection handling and add flight submission form
    Corrected connection error handling, added form for flight details submission, and improved SQL query handling.

--- airline.php
+++ airline.php
@@ -1,47 +1,81 @@
+<html>
+    <head></head>
+    <title></title>
+    <body>
+<form method="POST" action="#">
+     From<input type="text" name="fro"><br>
+     Airline<input type="text" name="airline"><br>
+     Departure <input type="date" name="dd"><br>
+     Arrival<input type="date" name="ad"><br><br>
+     Too<input type="text" name="too"><br><br>
+     Flight Number<input type="number" name="fn"><br><br>
+     Terminal<input type="number" name="ter"><br><br>
+    <input type="submit" name="submit">
+</form>
+</body>
+</html>
 <?php
 $servername="localhost";
 $username="root";
 $password="";
 $dbname="airdb";
 $tbname="air";
-$conn=mqsqli_connect($servername,$username,$password,$dbname);
-if(!conn)
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn)
 {
-    die("Connection Failed".mysqli_connect_error());
+die("Connection failed".mysqli_connect_error());
 }
-else{
-    echo"<br><h2 align=center>FLIGHT DETAILS</h2><br>";
+else
+{
+echo"<br><h2 align=center>FLIGHT DETAILS</h2><br/>";
 }
+
+$from=$_POST['fro'];
+$airlines=$_POST['airline'];
+$deptime=$_POST['dd'];
+$arrtime=$_POST['ad'];
+$to=$_POST['too'];
+$from=$_POST['fn'];
+$terminal=$_POST['ter'];
+$query="INSERT INTO air(fro,airline,dd,ad,too,fn,ter)VALUES('".$from."','".$airlines."','".$deptime."','".$arrtime."','".$to."','".$from."','".$terminal."')";
+$res=mysqli_query($conn,$query);
+if($res)
+{
+    echo"submitted succesfully";
+}
+else
+{
+    echo"error";
+}
+
+
 $sql="SELECT*FROM air";
 $res=mysqli_query($conn,$sql);
 if(mysqli_num_rows($res)>0)
 {
-    echo"<table border=2 align=center>";
-    echo"<tr><th>From</th>";
-    echo"<th>Airline</th>";
-    echo"<th>Departure Date</th>";
-    echo"<th>Arrival Date</th>";
-    echo"<th>To</th>";
-    echo"<th>Flight Number</th>";
-    echo"<th>Terminal</th></tr>";
-    while($row=mysqli_fetch_assoc($res))
-    {
-       echo"<tr><td>$row[From]</td>";
-       echo"<td>$row[Airline]</td>";
-       echo"<td>$row[DEPA]</td>";
-       echo"<td>$row[ad]</td>";
-       echo"<td>$row[too]</td>";
-       echo"<td>$row[fn]</td>";
-       echo"<td>$row[ter]</td></tr>";
-    }
+echo"<table border=2 align=center>";
+echo"<tr><th>From</th>";
+echo"<th>Airline</th>";
+echo"<th>Departure Date</th>";
+echo"<th>Arrival Date</th>";
+echo"<th>To</th>";
+echo"<th>Flight Number</th>";
+echo"<th>Terminal</th></tr>";
+while($row=mysqli_fetch_assoc($res))
+{
+echo"<tr><td>$row[fro]</td>";
+echo"<td>$row[airline]</td>";
+echo"<td>$row[dd]</td>";
+echo"<td>$row[ad]</td>";
+echo"<td>$row[too]</td>";
+echo"<td>$row[fn]</td>";
+echo"<td>$row[ter]</td></tr>";
+}
 echo"</table>";
 }
 else
 {
-    echo"0 result";
+echo"0 results";
 }
 mysqli_close($conn);
 ?>
-
-
-
